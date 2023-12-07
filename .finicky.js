b17778fce6366206a74a19c540346d4360ecdd87
Juan Cabrera <juan.terra@cencosud.com.uy>
2023-12-07 12:51:40 -0300

    chore: add some bare config files

---
+++ .finicky.js
@@ -0,0 +1,48 @@
+// Use https://finicky-kickstart.now.sh to generate basic configuration
+// Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
+
+// module.exports = {
+//   defaultBrowser: "Safari",
+//   handlers: [
+//     {
+//       // Open google.com and *.google.com urls in Google Chrome
+//       match: /^(https?:\/\/)?gitlab\.com.*$/,
+//       // match: ({ url }) => url.host === "gitlab.com",
+//       // match: /.*/,
+//       browser: {
+//         name: "Mozilla Firefox",
+//         profile: "Default",
+//       },
+//     },
+//     {
+//       match: /^(https?:\/\/)?cencosud\.atlassian\.net.*$/,
+//       // match: ({ url }) => url.host === "gitlab.com",
+//       // match: /.*/,
+//       browser: {
+//         name: "Google Chrome",
+//         profile: "Profile 3",
+//       },
+//     },
+//   ],
+// };
+module.exports = {
+    defaultBrowser: "Google Chrome",
+    handlers: [
+        {
+            match: /^https:\/\/gitlab.*$/,
+            browser: "Google Chrome",
+        },
+        {
+            match: /^https?:\/\/cencosud\.atlassian\.net\/.*$/,
+            browser: "Google Chrome",
+        },
+        {
+            match: /^https:\/\/device\.sso\.us\-east\-1\.amazonaws\.com\/$/,
+            browser: "Google Chrome",
+        },
+        {
+            match: /^https:\/\/statics\.teams\.cdn\.office\.net\/.*$/,
+            browser: "Google Chrome",
+        },
+    ],
+};
