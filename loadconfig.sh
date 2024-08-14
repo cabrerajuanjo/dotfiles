0c3b9028aa5596b9a181afd6b1bd442962936311
Juan Cabrera <cabrera57juanjo@gmail.com>
2024-08-14 12:58:57 -0300

    chore: dofiles notbare

---
+++ loadconfig.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Load configuration files
+
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.gitconfig-cencosud ~/.gitconfig-cencosud
+ln -sf ~/dotfiles/.gitconfig-personal ~/.gitconfig-personal
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.finicky.js ~/.finicky.js
+ln -sf ~/dotfiles/.config/kitty ~/.config
+ln -sf ~/dotfiles/.config/nvim ~/.config
+ln -sf ~/dotfiles/.config/tmux ~/.config
+# ln -sf .config/zsh ~/.config
+# ln -sf .config/omz/themes ~/.oh-my-zsh/custom
+
