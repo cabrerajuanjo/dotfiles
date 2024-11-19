e1ccdeda93716df4a001c5b93b441e9a1eca1247
Juan Cabrera <cabrera57juanjo@gmail.com>
2024-11-19 14:27:04 -0300

    chore: 191124

--- .config/zsh/tmux-sessionizer.sh
+++ .config/zsh/tmux-sessionizer.sh
@@ -6,7 +6,7 @@ else
 	# selected=$(find ~/work ~/work/spid -mindepth 1 -type d | fzf)
 	selected=$( (
 		# find ~/work ~/personal -maxdepth 2 -type d -not -path "*/node_modules*" -exec sh -c 'test -d "$1"/.git' -- {} \; -print -prune 2>/dev/null
-		fd --type d --max-depth 3 --exclude "node_modules" --full-path . ~/work ~/personal | while read -r dir; do
+		fdfind --type d --max-depth 3 --exclude "node_modules" --full-path . ~/work ~/personal | while read -r dir; do
 			if [ -d "$dir/.git" ]; then
 				echo "$dir"
 			fi
